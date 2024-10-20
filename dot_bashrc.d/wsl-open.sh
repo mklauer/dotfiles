0ca0138c88c393fc9bc6d8a1331b32fad16a4c5e
Matthias Klauer <klauer.matthias@gmail.com>
2024-10-20 18:06:41 +0200

    Config for wsl-open

---
+++ dot_bashrc.d/wsl-open.sh
@@ -0,0 +1,10 @@
+alias start=wsl-open
+
+# Adding wsl-open as a browser for Bash for Windows
+if [[ $(uname -r) =~ (m|M)icrosoft ]]; then
+  if [[ -z $BROWSER ]]; then
+    export BROWSER=wsl-open
+  else
+    export BROWSER=$BROWSER:wsl-open
+  fi
+fi
