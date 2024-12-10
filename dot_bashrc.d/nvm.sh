7d56dc989537099b58161dae634072aefad36c93
mklauer <klauer.matthias@gmail.com>
2024-12-10 11:08:47 +0100

    Remove dot_pofile

--- dot_bashrc.d/nvm.sh
+++ dot_bashrc.d/nvm.sh
@@ -1,3 +1,6 @@
-export NVM_DIR="$HOME/.nvm"
-[ -s "/home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh" ] && \. "/home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh"                                       # This loads nvm
-[ -s "/home/linuxbrew/.linuxbrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/home/linuxbrew/.linuxbrew/opt/nvm/etc/bash_completion.d/nvm" # This loads nvm bash_completion
+if [ -d "/home/linuxbrew/.linuxbrew/opt/nvm" ]; then
+  export NVM_DIR="$HOME/.nvm"
+  mkdir -p $NVM_DIR
+  [ -s "/home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh" ] && \. "/home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh"                                       # This loads nvm
+  [ -s "/home/linuxbrew/.linuxbrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/home/linuxbrew/.linuxbrew/opt/nvm/etc/bash_completion.d/nvm" # This loads nvm bash_completion
+fi
